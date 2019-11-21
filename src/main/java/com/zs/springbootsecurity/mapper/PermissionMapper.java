d5e4f5babda47b3a1c33cf63b960f1587c52c808
Zhai Shuo <38308786+github-zs@users.noreply.github.com>
2019-11-21 15:38:34 +0800

    add permission bo dao mapper

---
+++ src/main/java/com/zs/springbootsecurity/mapper/PermissionMapper.java
@@ -0,0 +1,23 @@
+package com.zs.springbootsecurity.mapper;
+
+import com.zs.springbootsecurity.bo.Permission;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface PermissionMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Permission record);
+
+    int insertSelective(Permission record);
+
+    Permission selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Permission record);
+
+    int updateByPrimaryKey(Permission record);
+    
+    List<Permission> selectByRole(Integer roleId);
+}
